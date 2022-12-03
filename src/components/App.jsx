628650a9ca289d5793770ec3e38b99565bb820ff
VladyslavLysenko <Dodog12362@gmail.com>
2022-12-03 19:42:12 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
+
+
+import React from 'react'
+// Main section App
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+        <p>Please leave feedback</p>
+          <Counter />
+          
+    </>
   );
 };
+// Counter
+class Counter extends React.Component{
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+
+    handleGood = (event) => {
+        console.log(this.state.good )
+        console.log(event.type)
+        this.setState(prevState => {
+            return {
+                good: prevState.good +1
+            }
+        })
+    }
+   
+    handleNeutral = (event) => {
+        console.log(this.state.neutral)
+        console.log(event.type)
+        this.setState(prevState => {
+            return {
+                neutral: prevState.neutral +1
+            }
+        })
+    }
+    handleBad = (event) => {
+        console.log(this.state.bad)
+        console.log(event.type)
+         this.setState(prevState => {
+            return {
+                bad: prevState.bad +1
+            }
+        })
+    }
+
+  render() {
+      return (
+          <>
+          <div className='FeedbackCounter'>
+              <button type='button' onClick={this.handleGood}>Good</button>
+              <button type='button' onClick={this.handleNeutral}>Neutral</button>
+              <button type='button' onClick={this.handleBad}>Bad</button>
+              </div>
+              <p>Statistics</p>
+              <ul className='Statistics'>
+                  <li><span>Good: {this.state.good}</span></li>
+                  <li> <span>Neutral: {this.state.neutral}</span></li>
+                  <li><span>Bad: {this.state.bad}</span></li>
+                  
+              </ul>
+        </>
+          
+
+    )
+  }
+}
---
+++ src/components/FeedbackCounter/FeedbackCounter.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// class Counter extends React.Component{
+//   render() {
+//     return (
+//       <div className='FeedbackCounter'>
+//         <button type='button' onClick={()=> {console.log("Good")}}>Good</button>
+//         <button type='button'  onClick={()=> {console.log("Neutral")}}>Neutral</button>
+//         <button type='button' onClick={()=> {console.log("Bad")}}>Bad</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Counter;
